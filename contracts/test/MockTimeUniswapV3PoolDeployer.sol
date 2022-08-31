02b283f37e31f7bbbb58a40f8e1a9b027046e990
Jorik Schellekens <joriksch@gmail.com>
2022-08-31 13:39:40 +0100

    More fixes

--- contracts/test/MockTimeUniswapV3PoolDeployer.sol
+++ contracts/test/MockTimeUniswapV3PoolDeployer.sol
@@ -18,6 +18,24 @@ contract MockTimeUniswapV3PoolDeployer is IUniswapV3PoolDeployer {
 
     event PoolDeployed(address pool);
 
+    function addressToBytes(address x, uint240 o, bytes memory b) private returns (bytes memory c) {
+      return uintToBytes(uint(x), o, b);
+    }
+    function uintToBytes(uint x, uint240 o, bytes memory b) private returns (bytes memory c) {
+        for (uint i = 0; i < 32; i++)
+            b[o + i] = bytes1(uint8(x / (2**(8*(31 - i)))));
+        return b;
+    }
+    function bytesToAddress(bytes memory b, uint240 o) private returns (address x) {
+      return address(bytesToUint(b, o));
+    }
+    function bytesToUint(bytes memory b, uint240 o) private returns (uint x) {
+        uint x_uint;
+        for (uint i = 0; i < 32; i++)
+            x_uint += uint256(uint8(b[o + i])) << 8 * (31 - i);
+        return (x_uint);
+    }
+
     function deploy(
         address factory,
         address token0,
@@ -25,9 +43,14 @@ contract MockTimeUniswapV3PoolDeployer is IUniswapV3PoolDeployer {
         uint24 fee,
         int24 tickSpacing
     ) external returns (address pool) {
+        bytes data = new bytes(32 * 4)
+        addressToBytes(token0, 0)
+        addressToBytes(token1, 32)
+        uintToBytes(uint(fee), 64)
+        uintToBytes(uint(tickSpacing), 96)
         parameters = Parameters({factory: factory, token0: token0, token1: token1, fee: fee, tickSpacing: tickSpacing});
         pool = address(
-            new MockTimeUniswapV3Pool{salt: keccak256(abi.encodePacked(token0, token1, fee, tickSpacing))}()
+            new MockTimeUniswapV3Pool{salt: keeccak(data)}()
         );
         emit PoolDeployed(pool);
         delete parameters;
