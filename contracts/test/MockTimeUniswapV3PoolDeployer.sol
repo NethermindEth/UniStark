d06e1196a4f7df1ad328e4c9e54da4c0e315b565
Jorik Schellekens <joriksch@gmail.com>
2022-10-04 12:33:30 +0100

    Use abi encode

--- contracts/test/MockTimeUniswapV3PoolDeployer.sol
+++ contracts/test/MockTimeUniswapV3PoolDeployer.sol
@@ -43,14 +43,14 @@ contract MockTimeUniswapV3PoolDeployer is IUniswapV3PoolDeployer {
         uint24 fee,
         int24 tickSpacing
     ) external returns (address pool) {
-        bytes memory data = new bytes(32 * 4);
-        addressToBytes(token0, 0, data);
-        addressToBytes(token1, 32, data);
-        uintToBytes(uint(fee), 64, data);
-        uintToBytes(uint(uint24(tickSpacing)), 96, data);
+        // bytes memory data = new bytes(32 * 4);
+        // addressToBytes(token0, 0, data);
+        // addressToBytes(token1, 32, data);
+        // uintToBytes(uint(fee), 64, data);
+        // uintToBytes(uint(uint24(tickSpacing)), 96, data);
         parameters = Parameters({factory: factory, token0: token0, token1: token1, fee: fee, tickSpacing: tickSpacing});
         pool = address(
-            new MockTimeUniswapV3Pool{salt: keccak256(data)}()
+            new MockTimeUniswapV3Pool{salt: keccak256(abi.encodePacked(token0, token1, fee, tickSpacing))}()
         );
         emit PoolDeployed(pool);
         delete parameters;
