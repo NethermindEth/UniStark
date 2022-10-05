b8f37b8bffdfeec0c73decbfdcac47e530898480
Swapnil Raj <raj.swapnil99@gmail.com>
2022-10-05 18:24:53 +0000

    Deployer uses abi.encode

--- contracts/UniswapV3PoolDeployer.sol
+++ contracts/UniswapV3PoolDeployer.sol
@@ -32,29 +32,8 @@ contract UniswapV3PoolDeployer is IUniswapV3PoolDeployer {
         int24 tickSpacing
     ) internal returns (address pool) {
         parameters = Parameters({factory: factory, token0: token0, token1: token1, fee: fee, tickSpacing: tickSpacing});
-        // pool = address(new UniswapV3Pool{salt: keccak256(abi.encode(token0, token1, fee))}());
-        pool = address(new UniswapV3Pool{salt: hash_stub_1(token0, token1, fee)}());
+        pool = address(new UniswapV3Pool{salt: keccak256(abi.encode(token0, token1, fee))}());
         delete parameters;
     }
 
-    /// warp-cairo
-    /// func CURRENTFUNC(){
-    ///    pedersen_ptr: HashBuiltin*,
-    ///    range_check_ptr: felt
-    ///}(
-    ///     token0 : felt,
-    ///     token1 : felt,
-    ///     fee : felt,
-    /// ) -> (res: Uint256){
-    ///     from starkware.cairo.common.hash import hash2
-    ///     from warplib.maths.utils import felt_to_uint256
-    ///     from starkware.cairo.common.cairo_builtins import HashBuiltin
-    ///     let (hash_res_1) = hash2{hash_ptr=pedersen_ptr}(token0, token1);
-    ///     let (hash_res_2) = hash2{hash_ptr=pedersen_ptr}(hash_res_1, fee);
-    ///     let (res: Uint256) = felt_to_uint256(hash_res_2);
-    ///     return (res,);
-    ///}
-    function hash_stub_1(address token0, address token1, uint24 fee) internal view returns (bytes32) {
-        return 0;
-    }
 }
