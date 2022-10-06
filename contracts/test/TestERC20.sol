73217be064d622773e51d35d27546367b4145a71
BitBaseBit <vardygreg23@gmail.com>
2022-10-06 18:20:31 +0100

    fix imports

--- contracts/test/TestERC20.sol
+++ contracts/test/TestERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.6;
 
-import './interfaces/IERC20Minimal.sol';
+import '../interfaces/IERC20Minimal.sol';
 
 contract TestERC20 is IERC20Minimal {
     mapping(address => uint256) public override balanceOf;
