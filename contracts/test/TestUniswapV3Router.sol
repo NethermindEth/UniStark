5ff1006b57b9420ee3768073110bb0c43b2c47f5
Jorik Schellekens <joriksch@gmail.com>
2022-10-06 19:19:42 +0100

    Support Router callback properly

--- contracts/test/TestUniswapV3Router.sol
+++ contracts/test/TestUniswapV3Router.sol
@@ -10,23 +10,6 @@ import '../interfaces/IUniswapV3Pool.sol';
 
 contract TestUniswapV3Router is IUniswapV3SwapCallback {
     using SafeCast for uint256;
-    function addressesToBytes(address x, address y) private returns (bytes memory b) {
-        b = new bytes(64);
-        for (uint i = 0; i < 32; i++)
-            b[i] = bytes1(uint8(uint(x) / (2**(8*(31 - i)))));
-        for (uint i = 0; i < 32; i++)
-            b[32 + i] = bytes1(uint8(uint(y) / (2**(8*(31 - i)))));
-    }
-    function bytesToAddresses(bytes memory b) private returns (address x, address y) {
-        uint x_uint;
-        uint y_uint;
-        for (uint i = 0; i < 32; i++)
-            x_uint += uint256(uint8(b[i])) << 8 * (31 - i);
-        for (uint i = 0; i < 32; i++)
-            y_uint += uint256(uint8(b[32 + i])) << 8 * (31 - i);
-        return (address(x_uint), address(y_uint));
-    }
-
 
     // flash swaps for an exact amount of token0 in the output pool
     function swapForExact0Multi(
@@ -40,7 +23,7 @@ contract TestUniswapV3Router is IUniswapV3SwapCallback {
             false,
             -amount0Out.toInt256(),
             TickMath.MAX_SQRT_RATIO - 1,
-            addressesToBytes(poolInput, msg.sender)
+            abi.encode(poolInput, msg.sender)
         );
     }
 
@@ -56,7 +39,7 @@ contract TestUniswapV3Router is IUniswapV3SwapCallback {
             true,
             -amount1Out.toInt256(),
             TickMath.MIN_SQRT_RATIO + 1,
-            addressesToBytes(poolInput, msg.sender)
+            abi.encode(poolInput, msg.sender)
         );
     }
 
@@ -69,26 +52,42 @@ contract TestUniswapV3Router is IUniswapV3SwapCallback {
     ) public override {
         emit SwapCallback(amount0Delta, amount1Delta);
 
-        (address pool, address payer) = bytesToAddresses(data);
+        (address pool, address payer) = abi.decode(data, (address, address));
+        // Note this test is changed because we do not support decoding pointer types yet
+        if (pool == address(0)) {
+            // get the address and amount of the token that we need to pay
+            address tokenToBePaid = IUniswapV3Pool(msg.sender).token1();
+            if (amount0Delta > 0) {
+                tokenToBePaid = IUniswapV3Pool(msg.sender).token0();
+            }
+            int256 amountToBePaid = amount1Delta;
+            if (amount0Delta > 0) {
+                amountToBePaid = amount0Delta;
+            }
 
-        // get the address and amount of the token that we need to pay
-        address tokenToBePaid = IUniswapV3Pool(msg.sender).token1();
-        if (amount0Delta > 0) {
-            tokenToBePaid = IUniswapV3Pool(msg.sender).token0();
+            bool zeroForOne = tokenToBePaid == IUniswapV3Pool(pool).token1();
+            IUniswapV3Pool(pool).swap(
+                msg.sender,
+                zeroForOne,
+                -amountToBePaid,
+                conditional(zeroForOne),
+                abi.encode(pool, payer)
+            );
+        } else {
+            if (amount0Delta > 0) {
+                IERC20Minimal(IUniswapV3Pool(msg.sender).token0()).transferFrom(
+                    payer,
+                    msg.sender,
+                    uint256(amount0Delta)
+                );
+            } else {
+                IERC20Minimal(IUniswapV3Pool(msg.sender).token1()).transferFrom(
+                    payer,
+                    msg.sender,
+                    uint256(amount1Delta)
+                );
+            }
         }
-        int256 amountToBePaid = amount1Delta;
-        if (amount0Delta > 0) {
-            amountToBePaid = amount0Delta;
-        }
-
-        bool zeroForOne = tokenToBePaid == IUniswapV3Pool(pool).token1();
-        IUniswapV3Pool(pool).swap(
-            msg.sender,
-            zeroForOne,
-            -amountToBePaid,
-            conditional(zeroForOne),
-            addressesToBytes(address(0), payer)
-        );
     }
 
     function conditional(bool zeroForOne) internal returns (uint160) {
