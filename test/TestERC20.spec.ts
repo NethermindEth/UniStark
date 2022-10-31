852f75c3a9b701a62d4702678088f6f8fd12e4fb
Guy <gprmccomb@gmail.com>
2022-10-31 11:38:55 +0000

    Adding test

---
+++ test/TestERC20.spec.ts
@@ -0,0 +1,37 @@
+import { expect } from './shared/expect'
+import { Wallet } from 'ethers'
+import { TestERC20 } from '../typechain/TestERC20'
+import { ethers, waffle } from 'hardhat'
+import snapshotGasCost from './shared/snapshotGasCost'
+
+const { BigNumber } = ethers
+
+describe('Testing Transfer', () => {
+  let erc20: TestERC20
+  
+  const fixture = async () => {
+    const erc20Factory = await ethers.getContractFactory('TestERC20')
+    return (await erc20Factory.deploy(0)) as TestERC20
+  }
+
+  before('deploying ERC20', async () => {
+    erc20 = await waffle.loadFixture(fixture)
+  })
+
+  it('transfer', async () => { 
+    const accounts = await ethers.getSigners();
+    const bob = accounts[1];
+    const alice = accounts[2];
+    
+    await erc20.mint(bob.address, 10101);
+    expect(await erc20.balanceOf(bob.address)).to.equal(10101);
+    expect(await erc20.balanceOf(alice.address)).to.equal(0);
+    
+
+    await erc20.connect(bob).transfer(alice.address, 10101);
+    expect(await erc20.balanceOf(bob.address)).to.equal(0);
+    expect(await erc20.balanceOf(alice.address)).to.equal(10101);
+    })
+
+ 
+})
--- test/UniswapV3Pool.swaps.spec.ts
+++ test/UniswapV3Pool.swaps.spec.ts
@@ -139,84 +139,84 @@ const DEFAULT_POOL_SWAP_TESTS: SwapTestCase[] = [
     exactOut: false,
     amount0: expandTo18Decimals(1),
   },
-  {
-    zeroForOne: false,
-    exactOut: false,
-    amount1: expandTo18Decimals(1),
-  },
-  {
-    zeroForOne: true,
-    exactOut: true,
-    amount1: expandTo18Decimals(1),
-  },
-  {
-    zeroForOne: false,
-    exactOut: true,
-    amount0: expandTo18Decimals(1),
-  },
-  // swap large amounts in/out with a price limit
-  {
-    zeroForOne: true,
-    exactOut: false,
-    amount0: expandTo18Decimals(1),
-    sqrtPriceLimit: encodePriceSqrt(50, 100),
-  },
-  {
-    zeroForOne: false,
-    exactOut: false,
-    amount1: expandTo18Decimals(1),
-    sqrtPriceLimit: encodePriceSqrt(200, 100),
-  },
-  {
-    zeroForOne: true,
-    exactOut: true,
-    amount1: expandTo18Decimals(1),
-    sqrtPriceLimit: encodePriceSqrt(50, 100),
-  },
-  {
-    zeroForOne: false,
-    exactOut: true,
-    amount0: expandTo18Decimals(1),
-    sqrtPriceLimit: encodePriceSqrt(200, 100),
-  },
-  // swap small amounts in/out
-  {
-    zeroForOne: true,
-    exactOut: false,
-    amount0: 1000,
-  },
-  {
-    zeroForOne: false,
-    exactOut: false,
-    amount1: 1000,
-  },
-  {
-    zeroForOne: true,
-    exactOut: true,
-    amount1: 1000,
-  },
-  {
-    zeroForOne: false,
-    exactOut: true,
-    amount0: 1000,
-  },
-  // swap arbitrary input to price
-  {
-    sqrtPriceLimit: encodePriceSqrt(5, 2),
-    zeroForOne: false,
-  },
-  {
-    sqrtPriceLimit: encodePriceSqrt(2, 5),
-    zeroForOne: true,
-  },
-  {
-    sqrtPriceLimit: encodePriceSqrt(5, 2),
-    zeroForOne: true,
-  },
-  {
-    sqrtPriceLimit: encodePriceSqrt(2, 5),
-    zeroForOne: false,
-  },
+  // {
+  //   zeroForOne: false,
+  //   exactOut: false,
+  //   amount1: expandTo18Decimals(1),
+  // },
+  // {
+  //   zeroForOne: true,
+  //   exactOut: true,
+  //   amount1: expandTo18Decimals(1),
+  // },
+  // {
+  //   zeroForOne: false,
+  //   exactOut: true,
+  //   amount0: expandTo18Decimals(1),
+  // },
+  // // swap large amounts in/out with a price limit
+  // {
+  //   zeroForOne: true,
+  //   exactOut: false,
+  //   amount0: expandTo18Decimals(1),
+  //   sqrtPriceLimit: encodePriceSqrt(50, 100),
+  // },
+  // {
+  //   zeroForOne: false,
+  //   exactOut: false,
+  //   amount1: expandTo18Decimals(1),
+  //   sqrtPriceLimit: encodePriceSqrt(200, 100),
+  // },
+  // {
+  //   zeroForOne: true,
+  //   exactOut: true,
+  //   amount1: expandTo18Decimals(1),
+  //   sqrtPriceLimit: encodePriceSqrt(50, 100),
+  // },
+  // {
+  //   zeroForOne: false,
+  //   exactOut: true,
+  //   amount0: expandTo18Decimals(1),
+  //   sqrtPriceLimit: encodePriceSqrt(200, 100),
+  // },
+  // // swap small amounts in/out
+  // {
+  //   zeroForOne: true,
+  //   exactOut: false,
+  //   amount0: 1000,
+  // },
+  // {
+  //   zeroForOne: false,
+  //   exactOut: false,
+  //   amount1: 1000,
+  // },
+  // {
+  //   zeroForOne: true,
+  //   exactOut: true,
+  //   amount1: 1000,
+  // },
+  // {
+  //   zeroForOne: false,
+  //   exactOut: true,
+  //   amount0: 1000,
+  // },
+  // // swap arbitrary input to price
+  // {
+  //   sqrtPriceLimit: encodePriceSqrt(5, 2),
+  //   zeroForOne: false,
+  // },
+  // {
+  //   sqrtPriceLimit: encodePriceSqrt(2, 5),
+  //   zeroForOne: true,
+  // },
+  // {
+  //   sqrtPriceLimit: encodePriceSqrt(5, 2),
+  //   zeroForOne: true,
+  // },
+  // {
+  //   sqrtPriceLimit: encodePriceSqrt(2, 5),
+  //   zeroForOne: false,
+  // },
 ]
 
 interface Position {
@@ -248,203 +248,203 @@ const TEST_POOLS: PoolTestCase[] = [
       },
     ],
   },
-  {
-    description: 'medium fee, 1:1 price, 2e18 max range liquidity',
-    feeAmount: FeeAmount.MEDIUM,
-    tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
-    startingPrice: encodePriceSqrt(1, 1),
-    positions: [
-      {
-        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
-  {
-    description: 'high fee, 1:1 price, 2e18 max range liquidity',
-    feeAmount: FeeAmount.HIGH,
-    tickSpacing: TICK_SPACINGS[FeeAmount.HIGH],
-    startingPrice: encodePriceSqrt(1, 1),
-    positions: [
-      {
-        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.HIGH]),
-        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.HIGH]),
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
-  {
-    description: 'medium fee, 10:1 price, 2e18 max range liquidity',
-    feeAmount: FeeAmount.MEDIUM,
-    tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
-    startingPrice: encodePriceSqrt(10, 1),
-    positions: [
-      {
-        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
-  {
-    description: 'medium fee, 1:10 price, 2e18 max range liquidity',
-    feeAmount: FeeAmount.MEDIUM,
-    tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
-    startingPrice: encodePriceSqrt(1, 10),
-    positions: [
-      {
-        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
-  {
-    description: 'medium fee, 1:1 price, 0 liquidity, all liquidity around current price',
-    feeAmount: FeeAmount.MEDIUM,
-    tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
-    startingPrice: encodePriceSqrt(1, 1),
-    positions: [
-      {
-        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        tickUpper: -TICK_SPACINGS[FeeAmount.MEDIUM],
-        liquidity: expandTo18Decimals(2),
-      },
-      {
-        tickLower: TICK_SPACINGS[FeeAmount.MEDIUM],
-        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
-  {
-    description: 'medium fee, 1:1 price, additional liquidity around current price',
-    feeAmount: FeeAmount.MEDIUM,
-    tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
-    startingPrice: encodePriceSqrt(1, 1),
-    positions: [
-      {
-        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        liquidity: expandTo18Decimals(2),
-      },
-      {
-        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        tickUpper: -TICK_SPACINGS[FeeAmount.MEDIUM],
-        liquidity: expandTo18Decimals(2),
-      },
-      {
-        tickLower: TICK_SPACINGS[FeeAmount.MEDIUM],
-        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
-  {
-    description: 'low fee, large liquidity around current price (stable swap)',
-    feeAmount: FeeAmount.LOW,
-    tickSpacing: TICK_SPACINGS[FeeAmount.LOW],
-    startingPrice: encodePriceSqrt(1, 1),
-    positions: [
-      {
-        tickLower: -TICK_SPACINGS[FeeAmount.LOW],
-        tickUpper: TICK_SPACINGS[FeeAmount.LOW],
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
-  {
-    description: 'medium fee, token0 liquidity only',
-    feeAmount: FeeAmount.MEDIUM,
-    tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
-    startingPrice: encodePriceSqrt(1, 1),
-    positions: [
-      {
-        tickLower: 0,
-        tickUpper: 2000 * TICK_SPACINGS[FeeAmount.MEDIUM],
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
-  {
-    description: 'medium fee, token1 liquidity only',
-    feeAmount: FeeAmount.MEDIUM,
-    tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
-    startingPrice: encodePriceSqrt(1, 1),
-    positions: [
-      {
-        tickLower: -2000 * TICK_SPACINGS[FeeAmount.MEDIUM],
-        tickUpper: 0,
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
-  {
-    description: 'close to max price',
-    feeAmount: FeeAmount.MEDIUM,
-    tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
-    startingPrice: encodePriceSqrt(BigNumber.from(2).pow(127), 1),
-    positions: [
-      {
-        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
-  {
-    description: 'close to min price',
-    feeAmount: FeeAmount.MEDIUM,
-    tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
-    startingPrice: encodePriceSqrt(1, BigNumber.from(2).pow(127)),
-    positions: [
-      {
-        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
-  {
-    description: 'max full range liquidity at 1:1 price with default fee',
-    feeAmount: FeeAmount.MEDIUM,
-    tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
-    startingPrice: encodePriceSqrt(1, 1),
-    positions: [
-      {
-        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        liquidity: getMaxLiquidityPerTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-      },
-    ],
-  },
-  {
-    description: 'initialized at the max ratio',
-    feeAmount: FeeAmount.MEDIUM,
-    tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
-    startingPrice: MAX_SQRT_RATIO.sub(1),
-    positions: [
-      {
-        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
-  {
-    description: 'initialized at the min ratio',
-    feeAmount: FeeAmount.MEDIUM,
-    tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
-    startingPrice: MIN_SQRT_RATIO,
-    positions: [
-      {
-        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-        liquidity: expandTo18Decimals(2),
-      },
-    ],
-  },
+  // {
+  //   description: 'medium fee, 1:1 price, 2e18 max range liquidity',
+  //   feeAmount: FeeAmount.MEDIUM,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //   startingPrice: encodePriceSqrt(1, 1),
+  //   positions: [
+  //     {
+  //       tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'high fee, 1:1 price, 2e18 max range liquidity',
+  //   feeAmount: FeeAmount.HIGH,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.HIGH],
+  //   startingPrice: encodePriceSqrt(1, 1),
+  //   positions: [
+  //     {
+  //       tickLower: getMinTick(TICK_SPACINGS[FeeAmount.HIGH]),
+  //       tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.HIGH]),
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'medium fee, 10:1 price, 2e18 max range liquidity',
+  //   feeAmount: FeeAmount.MEDIUM,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //   startingPrice: encodePriceSqrt(10, 1),
+  //   positions: [
+  //     {
+  //       tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'medium fee, 1:10 price, 2e18 max range liquidity',
+  //   feeAmount: FeeAmount.MEDIUM,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //   startingPrice: encodePriceSqrt(1, 10),
+  //   positions: [
+  //     {
+  //       tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'medium fee, 1:1 price, 0 liquidity, all liquidity around current price',
+  //   feeAmount: FeeAmount.MEDIUM,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //   startingPrice: encodePriceSqrt(1, 1),
+  //   positions: [
+  //     {
+  //       tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       tickUpper: -TICK_SPACINGS[FeeAmount.MEDIUM],
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //     {
+  //       tickLower: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //       tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'medium fee, 1:1 price, additional liquidity around current price',
+  //   feeAmount: FeeAmount.MEDIUM,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //   startingPrice: encodePriceSqrt(1, 1),
+  //   positions: [
+  //     {
+  //       tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //     {
+  //       tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       tickUpper: -TICK_SPACINGS[FeeAmount.MEDIUM],
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //     {
+  //       tickLower: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //       tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'low fee, large liquidity around current price (stable swap)',
+  //   feeAmount: FeeAmount.LOW,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.LOW],
+  //   startingPrice: encodePriceSqrt(1, 1),
+  //   positions: [
+  //     {
+  //       tickLower: -TICK_SPACINGS[FeeAmount.LOW],
+  //       tickUpper: TICK_SPACINGS[FeeAmount.LOW],
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'medium fee, token0 liquidity only',
+  //   feeAmount: FeeAmount.MEDIUM,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //   startingPrice: encodePriceSqrt(1, 1),
+  //   positions: [
+  //     {
+  //       tickLower: 0,
+  //       tickUpper: 2000 * TICK_SPACINGS[FeeAmount.MEDIUM],
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'medium fee, token1 liquidity only',
+  //   feeAmount: FeeAmount.MEDIUM,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //   startingPrice: encodePriceSqrt(1, 1),
+  //   positions: [
+  //     {
+  //       tickLower: -2000 * TICK_SPACINGS[FeeAmount.MEDIUM],
+  //       tickUpper: 0,
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'close to max price',
+  //   feeAmount: FeeAmount.MEDIUM,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //   startingPrice: encodePriceSqrt(BigNumber.from(2).pow(127), 1),
+  //   positions: [
+  //     {
+  //       tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'close to min price',
+  //   feeAmount: FeeAmount.MEDIUM,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //   startingPrice: encodePriceSqrt(1, BigNumber.from(2).pow(127)),
+  //   positions: [
+  //     {
+  //       tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'max full range liquidity at 1:1 price with default fee',
+  //   feeAmount: FeeAmount.MEDIUM,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //   startingPrice: encodePriceSqrt(1, 1),
+  //   positions: [
+  //     {
+  //       tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       liquidity: getMaxLiquidityPerTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'initialized at the max ratio',
+  //   feeAmount: FeeAmount.MEDIUM,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //   startingPrice: MAX_SQRT_RATIO.sub(1),
+  //   positions: [
+  //     {
+  //       tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
+  // {
+  //   description: 'initialized at the min ratio',
+  //   feeAmount: FeeAmount.MEDIUM,
+  //   tickSpacing: TICK_SPACINGS[FeeAmount.MEDIUM],
+  //   startingPrice: MIN_SQRT_RATIO,
+  //   positions: [
+  //     {
+  //       tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+  //       liquidity: expandTo18Decimals(2),
+  //     },
+  //   ],
+  // },
 ]
 
 describe('UniswapV3Pool swap tests', () => {
